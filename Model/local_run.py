e9e51be467934654fbb5d7afa149e4f4f8299b2d
Tekeh <te276@cam.ac.uk>
2021-01-24 11:17:14 +0000

    Minor rewrite as a function

--- Model/local_run.py
+++ Model/local_run.py
@@ -2,17 +2,21 @@ from encrypted_lin_reg import LocalOperations
 import numpy as np
 
 
-data_file = 'data'
-result_file = 'res'
-flag_file = 'flag'
-address = ''
-port=''
+def main(x=None,y=None, N=None):
+    data_file = 'data'
+    result_file = 'res'
+    flag_file = 'flag'
+    address = ''
+    port=''
+    N=10
 
+    x = np.arange(N, dtype=float)
+    y = beta_0*x + np.random.normal(size=N, scale=0.8)
+    x = x
 
-x = np.arange(N, dtype=float)
-y = learning_rate*(beta_0*x + np.random.normal(size=N, scale=0.8))
-x = learning_rate*x
+    data = np.stack([x,y], axis=0)
+    local_ops = LocalOperations(data_file, result_file, flag_file, address, port)
+    local_ops.process(data)
 
-data = np.stack([x,y], axis=0)
-local_ops = LocalOperations(data_file, result_file, flag_file, address, port)
-local_ops.process(data)
+if __name__=="__main__":
+    main()
