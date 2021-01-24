cfb4794f7ed3a73c9062265f723dfdaf6e18d52a
Irene-Li <gnituyil@gmail.com>
2021-01-24 11:58:24 +0000

    modify to send x and y data separately

--- Model/encrypted_lin_reg.py
+++ Model/encrypted_lin_reg.py
@@ -5,38 +5,40 @@ from p2p import Sender, Receiver
 
 class LocalOperations:
 
-    def __init__(self, data_file, result_file, flag_file, address, port):
+    def __init__(self, address, port):
         self.address = address
         self.port = port
-        self.data_file = data_file
-        self.result_file = result_file
-        self.flag_file = flag_file
         self.owner = DataOwner()
+        self.result_file = 'result'
+        self.flag_file = 'flag'
 
     def send(file_name):
         sender = Sender(self.address, self.port)
         sender.send(file_name)
         time.sleep(0.001)
-    
+
     def receive(file_name):
         receiver = Receiver(self.address, self.port)
         receiver.receive(file_name)
 
-    def process(self, data):
+    def process(self, x, y):
         flag = False
-        self._start(data)
+        self._start(x, y)
         while not flag:
             flag, result = self._listen()
             dec_result = self.owner.decrypt(result)
             if flag:
                 return dec_result
             else:
-                self._resend(data, dec_result)
+                self._resend(dec_result)
 
-    def _start(self, data):
-        enc_data = self.owner.encrypt(data)
-        self.owner.package_data(enc_data, self.data_file)
-        self.send(self.data_file)
+    def _start(self, x, y):
+        enc_x = self.owner.encrypt(x)
+        enc_y = self.owner.encrypt(y)
+        self.owner.package_data(x, 'x_data')
+        self.owner.package_data(y, 'y_data')
+        self.send('x_data')
+        self.send('y_data')
 
     def _listen(self):
         self.receive(self.result_file)
@@ -44,7 +46,7 @@ class LocalOperations:
         flag, result = self.owner.receive_results(self.result_file, self.flag_file)
         return flag, result
 
-    def _resend(self, data, dec_result):
+    def _resend(self, dec_result):
         enc_res = self.owner.encrypt(dec_result)
         self.owner.package_result(enc_res, self.result_file)
         self.send(self.result_file)
@@ -53,25 +55,24 @@ class LocalOperations:
 
 class EncryptedOperations:
 
-    def __init__(self, data_file, result_file, flag_file, address, port):
-        self.data_file = data_file
-        self.result_file = result_file
-        self.flag_file = flag_file
+    def __init__(self, address, port):
+        self.result_file = 'result'
+        self.flag_file = 'flag'
         self.address = address
         self.port = port
-    
+
     def send(file_name):
         sender = Sender(self.address, self.port)
         sender.send(file_name)
         time.sleep(0.001)
-    
+
     def receive(file_name):
         receiver = Receiver(self.address, self.port)
         receiver.receive(file_name)
 
     def _get_data(self):
         receiver = DataReceiver()
-        return receiver.unpack(self.data_file)
+        return receiver.unpack('x_data'), receiver.unpack('y_data')
 
     def _get_results(self):
         receiver = DataReceiver()
@@ -83,9 +84,9 @@ class EncryptedOperations:
         receiver.pack_results(results, self.result_file, flag, flag_file)
 
 class EncryptedLinReg(EncryptedOperations):
-    ## FInds minima via gradient descent
-    def __init__(self, data_file, result_file, flag_file, address, port):
-        super().__init__(data_file, result_file, flag_file, address, port)
+
+    def __init__(self, address, port):
+        super().__init__(address, port)
         # regression specific params
         self.count = 0 ## Number of operations
         self.beta = 0.5
@@ -97,7 +98,8 @@ class EncryptedLinReg(EncryptedOperations):
 
     def predict(self):
         ## Iterative procedure to get around lack of efficient inverses...
-        self.receive(self.data_file)
+        self.receive('x_data')
+        self.receive('y_data')
         self.enc_x, self.enc_y = self._get_data()
         self.err = np.empty(self.enc_x.size())
         for k in range(100): ## change with residual condition later
